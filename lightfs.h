b532a40c35d23f793fb86ad859fc3648f0a050c7
EricKim27 <seohyun.kim@outlook.kr>
2024-02-01 19:56:50 +0900

    2024-02-01 19:56 KST

--- lightfs.h
+++ lightfs.h
@@ -2,7 +2,7 @@
 #include <sys/types.h>
 
 //Superblock Structure
-struct superblock {
+struct lightfs_superblock {
     uint64_t inode_block_num;
     uint64_t total_block_count;
     uint64_t data_block_num;
@@ -18,14 +18,25 @@ struct superblock {
     uint16_t magicsig;
     uint16_t state;
 };
+//inode structure
 struct lightfs_inode {
     uint64_t inum;
     uint32_t imode;
-    uint32_t uid;
-    uint32_t gid;
-    
+    uint32_t access_time;
+    uint32_t modified_time;
+    uint32_t created_time;
+    uint32_t delete_time;
+    uint64_t direct_block_ptr[12];
+    uint64_t indirect_block_ptr[6];
+    uint64_t indirect_block_double[3];
+    uint16_t number_of_inodes;
+    uint16_t uid;
+    uint16_t gid;
 };
-struct dirblk_entry {
+//dir entry
+struct d_entry {
     uint64_t inode;
     char name[128];
 };
+
+extern int lightfs_readdir(char *name[128]);
