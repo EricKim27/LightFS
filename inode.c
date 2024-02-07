cab60232800e2557622ff837abd4fe590638eeb7
EricKim27 <seohyun.kim@outlook.kr>
2024-02-07 20:35:03 +0900

    added allocating inode function

--- inode.c
+++ inode.c
@@ -3,8 +3,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int allocate_inode(const char *device){
-    
+uint64_t allocate_inode(const char *device){
+    int device_f = open(device, O_RDWR);
+    if(device_f < 0){
+        perror("File open failed.");
+        return -1;
+    }
+    uint64_t number_of_inodes = sb_total_inode(device);
+    off_t start_bitmap = lseek(device_f, 1024 + sizeof(struct lightfs_superblock), SEEK_SET);
+    if(start_bitmap == -1){
+        perror("Failed to move to bitmap location.");
+        close(device_f);
+        return -1;
+    }
+    for(int i=0; i<number_of_inodes; i++){
+        char bitmap;
+        if (read(device_f, &bitmap, 1) == -1) {
+            perror("failed to read bitmap.");
+            close(device_f);
+            return -1;
+        }
+        if(bitmap == 0) {
+            return i;
+        }
+    }
+    printf("No free inodes available.\n");
+    close(device_f);
+    return -1;
 }
 int read_inode(struct lightfs_inode *inode, const char *device){
     int device_f = open(device, O_RDONLY);
@@ -13,14 +38,8 @@ int read_inode(struct lightfs_inode *inode, const char *device){
         return -1;
     }
     uint64_t number_of_inodes = sb_total_inode(device);
-    off_t start_inode = lseek(device_f, 1024 + sizeof(struct lightfs_superblock) + number_of_inodes, SEEK_SET);
+    off_t start_inode = lseek(device_f, 1024 + sizeof(struct lightfs_superblock) + (number_of_inodes / 8) + (sizeof(struct lightfs_inode) * inode->inum), SEEK_SET);
     if(start_inode == -1){
-        perror("Failed to move to inode location.");
-        close(device_f);
-        return -1;
-    }
-    off_t inode = lseek(device_f, sizeof(struct lightfs_inode) * inode->inum, SEEK_CUR);
-    if(inode == -1){
         perror("Failed to move to inode location.");
         close(device_f);
         return -1;
